5c5a11fe73384152888af67134212c0658e974b1
Josue_Klaysler <154643595+josuekla@users.noreply.github.com>
2025-01-06 22:06:03 -0300

    Added more instructions and method for addition and subtraction

--- calculadora_simples/main.py
+++ calculadora_simples/main.py
@@ -1,11 +1,32 @@
-class perguntas():
-	def pergunta_init(self):
-		main = input("Qual operacão matemática deseja?")
+class Calculadora():
+	def __init__(self):
+		instrucoes()
+		print("===== CALCULADORA SIMPLES EM PYTHON =====")
+		print("Escolha a operação matemática desejada: ")
+		print("1. Soma")
+		print("2. Subtração")
+		print("3. Multiplicação")
+		print("4. Divisão\n")
 		
+		def soma(self, *args):
+			print("Digite o numero para somar: ")
+			return sum(args)
+			
+		def subracao(self, *args):
+			resultado = valores[0]
+			for num in valores[1:]:
+				resultado -= num
+				return resultado
+
+		def pergunta_init(self):
+			main = input("Qual operacão matemática deseja?")
 		
 def instrucoes():
 	print("CALCULADORA SIMPLES EM PYTHON")
 	print("Faça o seu calculo matemático a seguir \n")
+	
+)
+def subtracao(*args)
 
 	
 	
