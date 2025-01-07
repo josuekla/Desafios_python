31c9badd9ea5cdfe80fb3c91aafdaa05e0f20b36
Josue_Klaysler <josueklayslerjk@gmail.com>
2025-01-07 13:17:27 -0300

    Add main function to display game categories in "jogo da forca"

--- jogo_da_foca/main.py
+++ jogo_da_foca/main.py
@@ -6,3 +6,21 @@ def palavras():
         'countries' : ['brasil', 'canada', 'alemanha', 'australia'],
         'colors' : ['vermelho', 'azul', 'amarelo', 'verde']
     }
+
+def main():
+    print('Bem vindo ao jogo da forca!')
+    print('Escolha uma categoria:')
+    print('1 - Profissões\n 2 - Empresas\n 3 - Filmes\n 4 - Países\n 5 - Cores')
+    category = input('Digite o número da categoria: ')
+    if category == '1':
+        print('Profissões')
+    elif category == '2':
+        print('Empresas')
+    elif category == '3':
+        print('Filmes')
+    elif category == '4':
+        print('Países')
+    elif category == '5':
+        print('Cores')
+    else:
+        print('Categoria inválida')
