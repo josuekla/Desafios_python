494c1a52f72ac40332331dc7ee2235f25e0bf698
Josue_Klaysler <josueklayslerjk@gmail.com>
2025-01-07 12:57:19 -0300

    Add word categories for jobs, companies, movies, countries, and colors

---
+++ jogo_da_foca/main.py
@@ -0,0 +1,8 @@
+def palavras():
+    words = {
+        'jobs' : ['dentista', 'programador', 'policial'],
+        'companies' : ['google', 'amazon', 'tesla', 'microsoft'],
+        'movies' : ['titanic', 'avatar', 'star wars', 'harry potter'],
+        'countries' : ['brasil', 'canada', 'alemanha', 'australia'],
+        'colors' : ['vermelho', 'azul', 'amarelo', 'verde']
+    }
