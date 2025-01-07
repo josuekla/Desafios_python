7f01d3ec6b8b06068241c528b3183acd5f294fbe
Josue_Klaysler <154643595+josuekla@users.noreply.github.com>
2025-01-06 21:23:08 -0300

    Added calculator launch instructions

---
+++ calculadora_simples/main.py
@@ -0,0 +1,14 @@
+class perguntas():
+	def pergunta_init(self):
+		main = input("Qual operacão matemática deseja?")
+		
+		
+def instrucoes():
+	print("CALCULADORA SIMPLES EM PYTHON")
+	print("Faça o seu calculo matemático a seguir \n")
+
+	
+	
+	
+instrucoes()
+calculo()
