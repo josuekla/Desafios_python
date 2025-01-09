aa5af0394d467a7b7e250cb8ea7189c17ba5d258
Josue_Klaysler <josueklayslerjk@gmail.com>
2025-01-08 21:12:06 -0300

    Adiciona opções de caracteres e escolha do usuário para números e símbolos

--- gerador_senhas_aleatorias/main.py
+++ gerador_senhas_aleatorias/main.py
@@ -1,3 +1,5 @@
+import string
+
 def gerador_senha():
     print(10*"==" + "Gerador de Senhas" + 10*"==")
 
@@ -9,3 +11,10 @@ def gerador_senha():
             print("A senha deve ser um número maior ou igual a 6.")
         except ValueError:
             print("Digite um número inteiro.")
+
+    letras = string.ascii_letters
+    number = string.digits
+    special = string.punctuation
+
+    incluir_numeros = input("Deseja incluir números na senha? (s/n): ").strip().lower()
+    incluir_especiais = input("Deseja incluir caracteres especiais na senha? (s/n): ").strip().lower()
